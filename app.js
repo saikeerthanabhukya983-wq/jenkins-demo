0c01de3e2b99e2ecf319b859a65e1ee2df8f7643
Keerthana <saikeerthanabhukya983@gmail.com>
2026-04-22 21:48:08 +0530

    updated message v2

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.write('Hello from Docker 🚀');
+    res.write('Hello from Kubernetes CI/CD 🚀 Version 2');
     res.end();
 });
 
-server.listen(3000, () => {
+server.listen(3000, '0.0.0.0', () => {
     console.log('Server running on port 3000');
 });
